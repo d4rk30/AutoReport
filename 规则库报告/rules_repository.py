2ebe6cd6b9a6325c71a76c2b08221daa583d3d39
d4rk30 <d4rk30@nohacker.me>
2025-07-09 23:33:55 +0800

    fix templter

--- 规则库报告/rules_repository.py
+++ 规则库报告/rules_repository.py
@@ -6,15 +6,15 @@ today = datetime.now().strftime('%Y-%m-%d')
 context_v8 = {
     'fileID': 'K01-policy-202509',
     'v8_test_time': today,
-    'v8_explanation': '此次在8.26801基础上新增了8条、优化了0条。此次规则更新在组件防护方面有显著效果',
-    'v8_rule1': '增加了请求中检测到金蝶Apusic中间件存在jndi注入漏洞',
-    'v8_rule2': '增加了请求中检测到泛微ecology9 FileDownloadLocation任意文件下载漏洞',
-    'v8_rule3': '增加了请求中检测到畅捷通T+ InitContext处存在登录绕过漏洞',
-    'v8_rule4': '增加了请求中检测到畅捷通T+ Save*InfoToFile处存在任意文件上传漏洞',
-    'v8_rule5': '增加了请求中检测到畅捷通T+ Get*All处存在敏感信息泄露漏洞',
-    'v8_rule_version': '8.26901',
-    'v8_bin_name': '8.26901.bin',
-    'v8_md5': '5bbaf353539d1b301176d78ce083c482'
+    'v8_explanation': '此次在8.26901基础上调整了5条规则，新增了5条。此次规则更新在组件防护方面有显著效果',
+    'v8_rule1': '增加了请求中检测到爱数AnyShare智能内容管理平台存在远程命令执行漏洞',
+    'v8_rule2': '增加了请求中检测到明源云erp存在认证绕过漏洞',
+    'v8_rule3': '增加了请求中检测到扁鹊飞救智能急救与质控系统GetLyfsByParams存在SQL注入漏洞',
+    'v8_rule4': '增加了请求中检测到章管家存在任意用户添加漏洞',
+    'v8_rule5': '增加了请求中检测到Unibox路由器pms_check.php存在远程命令执行漏洞',
+    'v8_rule_version': '8.27001',
+    'v8_bin_name': '8.27001.bin',
+    'v8_md5': '085f524802ed33dd99853f17bcc13cbd'
 }
 
 context_v9 = {
@@ -28,7 +28,7 @@ context_v9 = {
     'v9_rule5': '增加了请求中检测到Unibox路由器pms_check.php存在远程命令执行漏洞',
     'v9_rule_version': '9.270',
     'v9_bin_name': '9.270.bin',
-    'v9_md5': '5bbaf353539d1b301176d78ce083c482',
+    'v9_md5': '171b24ec5ab7434b1c7375d95b722529',
 }
 
 
@@ -45,4 +45,4 @@ def generate_report_v9():
 
 
 if __name__ == '__main__':
-    generate_report_v9()
+    generate_report_v8()
